ba7bdd15aaf16cb1d17689307729f49d13d52e7f
lbh2076 <leebh963@gmail.com>
2024-11-17 21:39:00 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -3,10 +3,10 @@ import geo.utils as utils
 # calculate the length of hypotenuse(c) when a=3 and b=4
 a, b = 3, 4
 c = utils.hypotenuse(a, b)  # hypotenuse 함수 호출
-print('c =', c)
+print("c =", c)
 
 # calculate the area of circle with radius r = 10
 r = 10
 area = utils.circle_area(r)  # circle_area 함수 호출
-print('area =', area)
+print("area =", area)
 
